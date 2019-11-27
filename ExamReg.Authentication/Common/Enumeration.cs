5ad6925dd58b56f6a8b9e967beb292c4dd3334a7
Anh Phan <anhphan.csf@gmail.com>
2019-11-27 22:48:30 +0700

    JWT enumeration

---
+++ ExamReg.Authentication/Common/Enumeration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExamReg.Authentication.Common
+{
+    public enum JWT
+    {
+        JWTSECRET = 0,
+        JWTLIFETIME = 1
+    }
+}
